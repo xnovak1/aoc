95a46466a15680a1ac048ac87cae83a1eba3043e
Michal Novák <michal.novak@seacomp.cz>
2023-12-04 09:51:28 +0100

    part 1 done

--- 2023/4/4-part-one.js
+++ 2023/4/4-part-one.js
@@ -10,7 +10,21 @@ const solve = () => {
   let result = 0;
 
   for (let i = 0; i < input.length; i++) {
+    let card_val = 0;
     const line = input[i].trim();
+    [_, nums] = line.split(":");
+    [winning_nums, our_nums] = nums.split("|");
+    let win = winning_nums.trim().split(/ +/);
+    let our = our_nums.trim().split(/ +/);
+
+    our.forEach((n) => {
+      if (win.includes(n)) {
+        card_val = card_val === 0 ? 1 : card_val * 2;
+      }
+    });
+
+    result += card_val;
+    console.log("line", i + 1, ":", card_val);
   }
 
   return result;
