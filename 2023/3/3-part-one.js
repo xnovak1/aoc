ae69a54408ea2ef19e8036c34de0cd9562cbd742
Michal Novák <michal.novak@seacomp.cz>
2023-12-04 13:00:32 +0100

    part 1 refactoring

--- 2023/3/3-part-one.js
+++ 2023/3/3-part-one.js
@@ -1,69 +1,73 @@
 const fs = require("fs");
 
 const input = fs
-    .readFileSync("input.txt")
-    .toString()
-    .split("\n")
-    .map((x) => x.trim());
+  .readFileSync("input.txt")
+  .toString()
+  .split("\n")
+  .map((x) => x.trim());
 
 let reg = new RegExp("\\d+", "g");
 
-const is_alphanumeric = (str) => {
-    for (let i = 0; i < str.length; i++) {
-        const code = str.charCodeAt(i);
-        if (str[i] !== "." &&
-            !(code > 47 && code < 58) && // numeric (0-9)
-            !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
-            return false;
-        }
-    }
+const is_alphanumeric = (char) => {
+  const code = char.charCodeAt(0);
+  return (
+    (code > 47 && code < 58) || // numeric (0-9)
+    (code > 64 && code < 91) || // upper alpha (A-Z)
+    (code > 96 && code < 123) // lower alpha (a-z)
+  );
+};
 
-    return true;
-}
+const is_dot = (char) => {
+  return char === ".";
+};
+
+const is_gear = (char) => {
+  return char === "*";
+};
 
 const has_symbol_neighbour = (lines, start_i, end_i, line_i) => {
-    const WIDTH = lines[0].length;
-    const HEIGHT = lines.length;
-    const num_len = end_i - start_i + 1;
+  const WIDTH = lines[0].length;
+  const HEIGHT = lines.length;
+  const num_len = end_i - start_i + 1;
 
-    for (let i = -1; i <= 1; i++) {
-        if ( (line_i + i < 0) || (line_i + i > HEIGHT - 1) ) {
-            continue;
-        }
-
-        for (let j = -1; j <= num_len; j++) {
-            if (start_i + j < 0 || start_i + j > WIDTH - 1) {
-                continue;
-            }
-
-            if (!is_alphanumeric(lines[line_i + i][start_i + j])) {
-                return true;
-            }
-        }
+  for (let i = -1; i <= 1; i++) {
+    if (line_i + i < 0 || line_i + i > HEIGHT - 1) {
+      continue;
     }
 
-    return false;
-}
+    for (let j = -1; j <= num_len; j++) {
+      if (start_i + j < 0 || start_i + j > WIDTH - 1) {
+        continue;
+      }
+
+      const char = lines[line_i + i][start_i + j];
+      if (!is_dot(char) && !is_alphanumeric(char)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
 
 const solve = () => {
-    let result = 0;
+  let result = 0;
 
-    for (let i = 0; i < input.length; i++) {
-        const line = input[i].trim();
-        let arr;
-        while ((arr = reg.exec(line)) != null) {
-            const num = arr[0];
-            const start_i = arr.index;
-            const end_i = start_i + num.length - 1;
+  for (let i = 0; i < input.length; i++) {
+    const line = input[i].trim();
+    let arr;
+    while ((arr = reg.exec(line)) != null) {
+      const num = arr[0];
+      const start_i = arr.index;
+      const end_i = start_i + num.length - 1;
 
-            if (has_symbol_neighbour(input, start_i, end_i, i)) {
-                result += Number(num);
-            }
-        }
+      if (has_symbol_neighbour(input, start_i, end_i, i)) {
+        result += Number(num);
+      }
     }
-    
-    return result;
-}
+  }
+
+  return result;
+};
 
 console.log(solve());
