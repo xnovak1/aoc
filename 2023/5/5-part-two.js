f0d1699c08be2bc5e3cae14adad7672f0abaa9f5
Michal Novák <michal.novak@seacomp.cz>
2023-12-05 17:00:32 +0100

    part 2 done (with little help)

--- 2023/5/5-part-two.js
+++ 2023/5/5-part-two.js
@@ -1,19 +1,66 @@
 const fs = require("fs");
 
-const input = fs
-  .readFileSync("small.txt")
+let input = fs
+  .readFileSync("input.txt")
   .toString()
-  .split("\n")
+  .split(/\r?\n\r?\n/)
   .map((x) => x.trim());
 
-const solve = () => {
-  let result = 0;
+let seeds_line = input[0];
 
-  for (let i = 0; i < input.length; i++) {
-    const line = input[i].trim();
+let inputs = seeds_line
+  .split(":")[1]
+  .trim()
+  .split(" ")
+  .map((x) => Number(x));
+
+let seeds = [];
+for (let i = 0; i < inputs.length; i += 2) {
+  seeds.push([inputs[i], inputs[i] + inputs[i + 1]]);
+}
+
+const solve = () => {
+  for (let i = 1; i < input.length; i++) {
+    let block = input[i];
+    let ranges = [];
+
+    const lines = block.split("\n");
+    for (let j = 1; j < lines.length; j++) {
+      let line = lines[j];
+      ranges.push(line.split(" ").map((x) => Number(x)));
+    }
+
+    let new_seeds = [];
+    while (seeds.length > 0) {
+      [seed_start, seed_end] = seeds.pop();
+      let found = false;
+      for (let k = 0; k < ranges.length; k++) {
+        [dest, start, interval] = ranges[k];
+        os = Math.max(seed_start, start);
+        oe = Math.min(seed_end, start + interval);
+        if (os < oe) {
+          new_seeds.push([os - start + dest, oe - start + dest]);
+          if (os > seed_start) {
+            seeds.push([seed_start, os]);
+          }
+          if (seed_end > oe) {
+            seeds.push([oe, seed_end]);
+          }
+
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        new_seeds.push([seed_start, seed_end]);
+      }
+    }
+
+    seeds = new_seeds;
   }
 
-  return result;
+  return Math.min(...seeds.flat());
 };
 
 console.log(solve());
